158129c538d27289b1b712b204696d4d1407b5fd
Paweł Świątkowski <katafrakt@vivaldi.net>
2023-04-17 00:01:13 +0200

    Add thread unpinning command

--- slices/moderation/repositories/thread.rb
+++ slices/moderation/repositories/thread.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Moderation::Repositories::Thread < Palaver::Repository[:threads]
+  def get(id)
+    threads.by_pk(id).one!
+  end
+
   def pin(thread_id)
     threads.by_pk(thread_id).changeset(:update, pinned: true).commit
   end
