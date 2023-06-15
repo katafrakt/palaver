d78e519becdae8303a3f86502271259390c83ded
Paweł Świątkowski <katafrakt@vivaldi.net>
2023-06-16 01:21:33 +0200

    Add specs for thread row and category row

---
+++ spec/slices/discussion/components/category_row_spec.rb
@@ -0,0 +1,14 @@
+require "nokolexbor"
+
+RSpec.describe Discussion::Components::CategoryRow do
+  include Phlex::Testing::ViewHelper
+
+  let(:category) { Discussion::Entities::Category.new(name: "Announcements", id: 24, thread_count: 25, message_count: 67) }
+
+  it "contains a link to a category" do
+    row = described_class.new(category:)
+    doc = Nokolexbor::HTML(render(row))
+    category_link = doc.xpath("//a").detect { |link| link.attributes["href"].to_s.include?(category_slug(category)) }
+    expect(category_link.attributes["href"].to_s).to eq("/cat/announcements-0Vh4")
+  end
+end
---
+++ spec/slices/discussion/components/thread_row_spec.rb
@@ -0,0 +1,14 @@
+require "nokolexbor"
+
+RSpec.describe Discussion::Components::ThreadRow do
+  include Phlex::Testing::ViewHelper
+
+  let(:thread) { Discussion::Entities::Thread.new(title: "Registrations are now closed", id: 22, message_count: 1, pinned: false, messages: []) }
+
+  it "contains a link to a category" do
+    row = described_class.new(thread)
+    doc = Nokolexbor::HTML(render(row))
+    thread_link = doc.xpath("//a").detect { |link| link.attributes["href"].to_s.include?(thread_slug(thread)) }
+    expect(thread_link.attributes["href"].to_s).to eq("/th/registrations-are-now-closed-BNtg")
+  end
+end
