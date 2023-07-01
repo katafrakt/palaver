107072133e2195335f93cdcbb8d95586541cb516
yuszuv <jan@sternprodukt.de>
2023-07-01 19:59:32 +0200

    add slugs to reply form and pagination

--- slices/discussion/actions/thread/reply.rb
+++ slices/discussion/actions/thread/reply.rb
@@ -4,15 +4,17 @@ class Discussion::Actions::Thread::Reply < Discussion::Action
   include Discussion::Deps[
     reply: "commands.add_message",
     repo: "repositories.thread",
-    profile_repo: "repositories.profile"
+    profile_repo: "repositories.profile",
+    slugger: "utils.slugger"
   ]
 
   def handle(req, res)
-    thread = repo.get(req.params[:id])
+    thread = repo.get(slugger.decode_id(req.params[:id]))
     profile = profile_repo.get(res[:current_user].profile_id)
     reply.call(thread:, author: profile, content: req.params[:reply])
+    slug = slugger.to_slug(Discussion::Entities::Thread::HASHIDS_NUM, thread.title, thread.id)
 
     # TODO: redirect always to last page and add anchor
-    res.redirect_to "/th/#{thread.id}"
+    res.redirect_to "/th/#{slug}"
   end
 end
