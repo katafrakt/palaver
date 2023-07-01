8308cc27669084b1740ca38a22dca5478ba58fb1
yuszuv <jan@sternprodukt.de>
2023-07-01 23:38:40 +0200

    expand category show action's specs

--- spec/slices/discussion/actions/category/show_spec.rb
+++ spec/slices/discussion/actions/category/show_spec.rb
@@ -1,15 +1,36 @@
 RSpec.describe Discussion::Actions::Category::Show do
   subject(:action) { described_class.new }
   let(:repo) { double(:repo) }
+  let(:query) { double(:query) }
   stub(Discussion::Container, "repositories.category") { repo }
+  stub(Discussion::Container, "queries.threads_in_category") { query }
 
-  describe "with no categories" do
-    it "renders message about no threads" do
-      category = Factory.structs[:category]
+  describe "with no threads" do
+    let(:category) { Factory.structs[:category] }
+
+    before do
       expect(repo).to receive(:get) { category }
+      expect(query).to receive(:call).with(category.id) { [] }
+    end
 
+    it "renders message about no threads" do
       response = action.call({id: category_slug(category)})
       expect(response.body.first).to include("No threads")
+    end
+  end
+
+  describe "with threads" do
+    let(:category) { Factory[:category] }
+    let(:threads) { [Discussion::Entities::Thread.from_rom(category.latest_thread)] }
+
+    before do
+      expect(repo).to receive(:get) { category }
+      expect(query).to receive(:call).with(category.id) { threads }
+    end
+
+    it "renders message about no threads" do
+      response = action.call({id: category_slug(category)})
+      threads.each { |th| expect(response.body.first).to include(th.title) }
     end
   end
 end
