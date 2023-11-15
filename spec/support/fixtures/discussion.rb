65eca8158fda0d1c7d36523ee605e61fa438d46c
Paweł Świątkowski <katafrakt@vivaldi.net>
2023-11-15 23:01:48 +0100

    Don't crash on profile page when user does not have a profile set up

--- spec/support/fixtures/discussion.rb
+++ spec/support/fixtures/discussion.rb
@@ -26,12 +26,12 @@ module Fixtures
       ::Discussion::Container["repositories.thread"].handle_event(event)
     end
 
-    def profile
+    def profile(account_id: 1)
       # there's a cross-slice dependency here, but I guess it is okay in test code
       # Discussion slice does not have the rights to write profiles, but they are
       # needed to read.
       repo = ::Account::Container["repositories.profile"]
-      record = repo.create(nickname: "test", account_id: 1)
+      record = repo.create(nickname: "test", account_id:)
       ::Discussion::Entities::Profile.new(record.to_h.merge(message_count: 1))
     end
   end
