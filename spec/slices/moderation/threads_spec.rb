3eb425b3aa4ddf9c2f67af531155037da071347c
Paweł Świątkowski <katafrakt@vivaldi.net>
2024-01-27 21:41:54 +0100

    Allow moderators to lock threads

--- spec/slices/moderation/threads_spec.rb
+++ spec/slices/moderation/threads_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Moderation::Threads do
   let(:threads) { Moderation::Container["threads"] }
 
   def build_thread(args = {})
-    params = {id: rand(100), title: "My test thread", pinned: false}.merge(args)
+    params = {id: rand(100), title: "My test thread", pinned: false, locked: false}.merge(args)
     Moderation::Entities::Thread.new(params)
   end
 
@@ -40,6 +40,46 @@ RSpec.describe Moderation::Threads do
       result = threads.unpin(build_thread)
       expect(result).to be_failure
       expect(result.failure).to eq(:thread_not_pinned)
+    end
+  end
+
+  describe "lock" do
+    it "returns success for thread" do
+      thread = build_thread
+      expect(threads.lock(thread)).to be_success
+    end
+
+    it "returns correct event on success" do
+      thread = build_thread
+      event = threads.lock(thread).value!
+      expect(event.thread_id).to eq(thread.id)
+    end
+
+    it "returns failure if thread if already locked" do
+      thread = build_thread(locked: true)
+      result = threads.lock(thread)
+      expect(result).to be_failure
+      expect(result.failure).to eq(:thread_already_locked)
+    end
+  end
+
+  describe "unlock" do
+    it "returns success for locked thread" do
+      thread = build_thread(locked: true)
+      expect(threads.unlock(thread)).to be_success
+    end
+
+    it "returns correct event on success" do
+      thread = build_thread(locked: true)
+      event = threads.unlock(thread).value!
+      expect(event.thread_id).to eq(thread.id)
+    end
+
+    it "returns failure when thread is unlcoked" do
+      thread = build_thread(locked: false)
+      result = threads.unlock(thread)
+      expect(result).to be_failure
+      expect(result.failure).to eq(:thread_not_locked)
     end
   end
 end
