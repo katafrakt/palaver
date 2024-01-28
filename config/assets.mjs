adf0f43425f1da552b0b603dcca7a80070bee26d
Paweł Świątkowski <katafrakt@vivaldi.net>
2024-01-28 20:49:24 +0100

    Bundle Bulma instead of using CDN

--- config/assets.mjs
+++ config/assets.mjs
@@ -1,14 +1,10 @@
 import * as assets from "hanami-assets";
+import {sassPlugin} from 'esbuild-sass-plugin'
 
-await assets.run();
-
-// To provide additional esbuild (https://esbuild.github.io) options, use the following:
-//
-// await assets.run({
-//   esbuildOptionsFn: (args, esbuildOptions) => {
-//     // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
-//     // compile vs watch.
-//
-//     return esbuildOptions;
-//   }
-// });
+await assets.run({esbuildOptionsFn: (args, esbuildOptions) => {
+  esbuildOptions.plugins.push(sassPlugin({
+    filter: /\.scss$/
+  }));
+  
+  return esbuildOptions;
+ }});
