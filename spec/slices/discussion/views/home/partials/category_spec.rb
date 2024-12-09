c95da571f97c401ff37b4d4d4cfb90e05466d912
Paweł Świątkowski <katafrakt@vivaldi.net>
2024-12-09 14:01:04 +0100

    Add spec for last message info in category partial

--- spec/slices/discussion/views/home/partials/category_spec.rb
+++ spec/slices/discussion/views/home/partials/category_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Discussion::Views::Home::Partials::Category do
     category_link = doc.xpath("//a").detect { |link| link.attributes["href"].to_s.include?(category_slug(category)) }
     expect(category_link.attributes["href"].to_s).to eq("/cat/announcements-0Vh4")
   end
+
+  it "displays informatio about last message and its author" do
+    author = Discussion::Entities::Author.new(nickname: "John", id: 123, account_id: 17)
+    thread = Discussion::Entities::Thread.new(id: 1, title: "Welcome thread", author:)
+    row = described_class.new(category: Discussion::Entities::Category.new(category.attributes.merge(latest_thread: thread)))
+    doc = Nokolexbor::HTML(render(row))
+    profile_link = doc.xpath("//a").detect { |link| link.attributes["href"].to_s.include?("th/") }
+    expect(profile_link).to_not be_nil
+  end
 end
