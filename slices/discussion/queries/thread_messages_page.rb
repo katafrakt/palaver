bdb4b5017869d859b0eae9d21ec0bd60f19b5aa8
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-06-13 09:09:32 +0200

    Don't show reply form unless we are on the last page

--- slices/discussion/queries/thread_messages_page.rb
+++ slices/discussion/queries/thread_messages_page.rb
@@ -6,12 +6,13 @@ class Discussion::Queries::ThreadMessagesPage
   Pager = Struct.new(:entries, :total_entries, :total_pages, :current_page)
 
   def call(thread_id, page_no)
+    messages_per_page = ENV["DEFAULT_MESSAGES_PER_THREAD_PAGE"] || 15
     thread = repo.get(thread_id)
     thread = Discussion::Entities::Thread.new(thread.to_h)
     all_thread_messages_count = repo.message_counts([thread_id]).first.count
     entries = repo.paged_messages(thread_id, page_no.to_i)
     entries.map! { |e| Discussion::Entities::Message.from_rom(e) }
-    pager = Pager.new(entries, all_thread_messages_count, (all_thread_messages_count / 15.0).ceil, page_no.to_i)
+    pager = Pager.new(entries, all_thread_messages_count, (all_thread_messages_count / messages_per_page.to_f).ceil, page_no.to_i)
 
     {thread:, pager:}
   end
