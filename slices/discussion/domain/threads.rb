0008b899d778993644f265c9fb536e112f63fa0c
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-06-17 23:52:22 +0200

    Change start_thread to operation

--- slices/discussion/domain/threads.rb
+++ slices/discussion/domain/threads.rb
@@ -4,10 +4,6 @@ module Discussion
       include Dry::Monads[:result]
       include Discussion::Events
 
-      def start_thread(category:, title:, content:, creator:)
-        ThreadCreated.new(title:, content:, creator:, category_id: category.id)
-      end
-
       def add_reply(thread, author:, content:)
         return Failure(:thread_locked) if thread.locked?
 
