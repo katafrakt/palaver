6c732545793202fb415bdee6980c1cafdb1a0a95
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-06-18 00:03:40 +0200

    Transform reply_to_thread to operation

--- slices/discussion/actions/thread/reply.rb
+++ slices/discussion/actions/thread/reply.rb
@@ -2,19 +2,18 @@
 
 class Discussion::Actions::Thread::Reply < Discussion::Action
   include Discussion::Deps[
+    "operations.reply_to_thread",
     repo: "repositories.thread",
-    profile_repo: "repositories.profile",
-    slugger: "utils.slugger",
-    threads: "domain.threads"
+    slugger: "utils.slugger"
   ]
 
   def handle(req, res)
     thread_id = slugger.decode_id(req.params[:id])
     thread = repo.get(thread_id)
     author = res[:current_user].to_author
-    case threads.add_reply(thread, author:, content: req.params[:reply])
+
+    case reply_to_thread.call(thread:, author:, content: req.params[:reply])
     in Success(event)
-      repo.handle(event)
       slug = slugger.to_slug(Discussion::Entities::Thread::HASHIDS_NUM, thread.title, thread.id)
 
       # TODO: redirect always to last page and add anchor
