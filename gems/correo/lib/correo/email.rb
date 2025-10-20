e8e48bc5bc9440446bc484d0741fc61f00ec21a4
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-10-20 21:58:14 +0200

    Improve email API to avoid weird build overrides

--- gems/correo/lib/correo/email.rb
+++ gems/correo/lib/correo/email.rb
@@ -7,29 +7,29 @@ module Correo
 
     class_attribute :html_template
 
-    def to
+    def to(*)
       raise NotImplementedError
     end
 
-    def from
+    def from(*)
       raise NotImplementedError
     end
 
-    def subject
+    def subject(*)
       raise NotImplementedError
     end
 
     def html_part = nil
 
-    def build(args)
+    def build(**args)
       # if class.html_template s not avaiable, this could fall back to Hanami's default
       # template lookup logic
       html_part = self.class.html_template.new(**args).call if self.class.html_template
 
       Mail.new.tap do |mail|
-        mail.to = to
-        mail.from = from
-        mail.subject = subject
+        mail.to = to(**args)
+        mail.from = from(**args)
+        mail.subject = subject(**args)
 
         if html_part
           mail.html_part = Mail::Part.new do
