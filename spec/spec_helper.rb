5ea23c41bb094ee327b229802d15376216e3a70b
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-10-20 22:08:20 +0200

    Don't count coverage for gems directory
    Gems should be stadalone and have their own coverage checks, if desired.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ SimpleCov.start do
   primary_coverage :branch
   add_filter "/spec/"
   add_filter "/config/"
+  add_filter "/gems/"
   add_group "Account", "slices/account"
   add_group "Discussion", "slices/discussion"
   add_group "UI", "lib/ui"
